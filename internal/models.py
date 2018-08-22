0ed7278ca6544f2970f18bd3e7af1170a208ca2e
zootos <halvard.hummel@hotmail.com>
2018-08-22 14:18:57 +0200

    Initial member model

--- internal/models.py
+++ internal/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+
+    email = models.fields.EmailField
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.fields.CharField(max_length=32)
+    phone_number = models.fields.IntegerField
+    study_program = models.CharField(max_length=32)
+    card_number = models.CharField(max_length=32)
+    active = models.BooleanField
+
+    class Meta:
+        permissions = (
+            ("is_internal", "Is a member of MAKE NTNU"),
+        )
